4a8d0f38189a945b573355833566bdab46ce55bd
ZamanSafarov <zamanjs@code.edu.az>
2022-01-21 15:18:13 +0400

    .

--- Tasks/task 5/Program.cs
+++ Tasks/task 5/Program.cs
@@ -7,7 +7,7 @@ namespace task_5
         static void Main(string[] args)
         {
             //1-50 arasi ancaq 5-e ya da 3-e bolunen ededleri cap etmek (misal ucun 3,5,9,12,15)
-            for (int i = 0; i < 50; i++)
+            for (int i = 1; i < 50; i++)
             {
                 if (i % 3 == 0) 
                 {
